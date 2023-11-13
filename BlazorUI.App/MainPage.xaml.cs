0bfae18d30426723f73476a5810b91850bd1dacd
Omar Torres <jtorres@axsistec.com>
2023-11-13 11:11:52 -0600

    Add project files.

---
+++ BlazorUI.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorUI.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlazorUI.SharedCode/Extensions/ServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+
+using BlazorUI.SharedCode.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorUI.SharedCode.Extensions
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection  AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<WeatherForecastService>();
+
+            return services;
+        }
+
+    }
+}
